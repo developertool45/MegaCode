4644e17dd9b41a91bcb89df44affe28acac083ee
developertool45 <developertool45@gmail.com>
2025-07-05 18:27:38 +0530

    update

--- src/app.js
+++ src/app.js
@@ -20,9 +20,19 @@ import subTasksRoutes from './routes/subTasks.routes.js';
 const whitelist = [
 	'https://tasks-frontend-dwjf.vercel.app', 	                 
 	'http://localhost:5173',
-  ];
+];
+  
+const corsOptions = {
+	origin: (origin, callback) => {
+		if (whitelist.includes(origin) || !origin) {
+			callback(null, true);
+		} else {
+			callback(new Error('Not allowed by CORS'));
+		}
+	},
+}
 app.use(cors({
-	origin: [process.env.CORS_ORIGIN || "http://localhost:5173"],
+	// origin: [process.env.CORS_ORIGIN || "http://localhost:5173"],
 	origin(origin, cb) {		
 		if (!origin || whitelist.includes(origin)) return cb(null, true);
 		return cb(new Error('Not allowed by CORS'));
@@ -32,7 +42,7 @@ app.use(cors({
 	credentials: true
 }));
 
-app.options('*', cors());
+app.options('*', cors(corsOptions));
   
 // middleware for form and json data
 app.use(express.json({limit: '10kb'}));
