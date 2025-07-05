ec0925402578f9519786e6694821a26099dc844a
developertool45 <developertool45@gmail.com>
2025-07-05 23:53:11 +0530

    update cookies

--- src/utils/contants.js
+++ src/utils/contants.js
@@ -1,29 +1,27 @@
 export const UserRolesEnum = {
-    ADMIN: 'admin',
-    PROJECT_ADMIN: "project_admin",
-    MEMBER : "member"
-}
+  ADMIN: 'admin',
+  PROJECT_ADMIN: 'project_admin',
+  MEMBER: 'member',
+};
 export const AvailableUserRoles = Object.values(UserRolesEnum);
 
 export const TaskStatusEnum = {
-    TODO: "todo",
-    IN_PROGRESS: "in_progress",
-    Done: 'done'
-}
+  TODO: 'todo',
+  IN_PROGRESS: 'in_progress',
+  Done: 'done',
+};
 export const AvailableTaskStatus = Object.values(TaskStatusEnum);
 
 export const ProjectStatusEnum = {
-    pending: "pending",
-    in_progress: "in_progress",
-    completed: "completed"
-}
+  pending: 'pending',
+  in_progress: 'in_progress',
+  completed: 'completed',
+};
 export const AvailableProjectStatus = Object.values(ProjectStatusEnum);
 
-
 export const options = {
-    httpOnly: true,
-    secure: true,
-    sameSite: "lax",
-    maxAge: 24 * 60 * 60 * 1000,
-    expires: new Date(Date.now() + (24 * 60 * 60 * 1000))
-}
+  httpOnly: true,
+  secure: true,
+  sameSite: 'None',
+  maxAge: 24 * 60 * 60 * 1000,
+};
