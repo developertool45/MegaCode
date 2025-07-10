0ed9236d13694c7499e16843e869021f63a83dbd
developertool45 <developertool45@gmail.com>
2025-07-10 18:17:50 +0530

    update isAdmin

--- src/middleware/isAdmin.middleware.js
+++ src/middleware/isAdmin.middleware.js
@@ -2,6 +2,7 @@ import { ApiError } from "../utils/api-errors.js";
 import { asyncHandler } from "../utils/async-handler.js";
 import { User } from "../models/user.models.js";
 import { UserRolesEnum } from "../utils/contants.js";
+import { ProjectMember } from "../models/projectmember.models.js";
 
 export const isAdmin = asyncHandler(async (req, res, next) => {
 	console.log("=====isAdmin middleware=====");
@@ -15,7 +16,31 @@ export const isAdmin = asyncHandler(async (req, res, next) => {
 		throw new ApiError(404, "User not found!");
 	}
 	if (user.role !== UserRolesEnum.ADMIN) {
-		throw new ApiError(403, "You are not Authorized to perform this action!");
+		const projecetMember = await ProjectMember.findOne({
+			project: req.params.projectId,
+			user: userId
+		})
+
+		if (!projecetMember) {
+			throw new ApiError(403, "You are not Authorized to perform this action!");
+		}
+		if (projecetMember.role !== UserRolesEnum.ADMIN) {
+			throw new ApiError(403, "You are not Authorized to perform this action!");
+		}
+	} else {
+		if (req.params.projectId) {
+			const projectMember = await ProjectMember.findOne({
+				project: req.params.projectId,
+				user: userId
+			})
+			if (!projectMember) {
+				throw new ApiError(403, "You are not Authorized to perform this action!");
+			}
+			if (projectMember.role !== UserRolesEnum.ADMIN) {
+				throw new ApiError(403, "You are not Authorized to perform this action!");
+			}
+	}
+		
 	}
 	next();
 });
